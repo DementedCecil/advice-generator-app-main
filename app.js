02a3ebbbfd1a7798f6359815248852c185250f31
John Caswell <shalindria@rocketmail.com>
2023-06-29 22:27:31 +0100

    .

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ const adviceNo = document.getElementById('advice-no');
 const adviceText = document.getElementById('advice-text');
 const dice = document.getElementById('dice');
 
+let min = 1;
+let max = 224;
+
+function getRandom() {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 dice.addEventListener("click", getAdvice);
 
+// Advice IDs are from 1 to 224
+// Seems to be cached when being called from a site, though via PostMan
+// it works fine
+// Using a random number within the call instead allows this to work
+
 async function getAdvice() {
-    let response = await fetch('https://api.adviceslip.com/advice');
+    let randomNo = getRandom();
+
+    let response = await fetch(`https://api.adviceslip.com/advice/${randomNo}`);
 
 	if (!response.ok) {
 		throw new Error(`HTTP error! status: ${response.status}`);
